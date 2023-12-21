fc651eb4a76aab3f508e7256c4971ff9d4480f8a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-12-21 19:41:05 +0600

    Added endpoints to update and delete tasks

--- Index.js
+++ Index.js
@@ -32,19 +32,58 @@ async function run() {
     // API endpoint to insert task data to the database
     app.post("/add-task", async (req, res) => {
       let data = req.body;
-
       await tasksCollection.insertOne(data);
-
       res.send({ success: true });
     });
 
     // API endpoint to get specific tasks filtered by email
-    app.post("/my-tasks", async (req, res) => {
+    app.get("/my-tasks", async (req, res) => {
       let email = req.query.email;
-
       let result = await tasksCollection.find({ email: email }).toArray();
-
       res.send(result);
+    });
+
+    // API endpoint to delete a task
+    app.delete("/delete-task", async (req, res) => {
+      const taskId = req.query.id;
+
+      if (!ObjectId.isValid(taskId)) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid task ID" });
+      }
+
+      const objectId = new ObjectId(taskId);
+      const result = await tasksCollection.deleteOne({ _id: objectId });
+
+      if (result.deletedCount === 1) {
+        res.json({ success: true, message: "Task deleted successfully" });
+      } else {
+        res.status(404).json({ success: false, message: "Task not found" });
+      }
+    });
+
+    // API endpoint to update a task data
+    app.post("/update-task", async (req, res) => {
+      let data = req.body;
+
+      const taskId = req.body._id;
+      const updatedData = req.body;
+
+      const updatedTask = await tasksCollection.findOneAndUpdate(
+        { _id: new ObjectId(taskId) },
+        {
+          $set: {
+            title: updatedData.title,
+            description: updatedData.description,
+            deadline: updatedData.deadline,
+            priority: updatedData.priority,
+          },
+        },
+        { returnDocument: "after" }
+      );
+
+      res.send(updatedTask);
     });
   } finally {
   }
